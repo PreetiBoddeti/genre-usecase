3b71db3cc9dc73842cb0b7fc8881ad53a638d341
PreetiBoddeti <iteerp999@gmail.com>
2020-09-24 14:00:28 +0530

    added few apis

--- app.js
+++ app.js
@@ -10,70 +10,418 @@ const debugpost = require("debug")("genre-project:POST");
 const debugput = require("debug")("genre-project:PUT");
 const debugdelete = require("debug")("genre-project:DELETE");
 const cors = require("cors");
-app.use(cors());
-
-
+const mongoose = require("mongoose");
+const { Mongoose } = require("mongoose");
+app.use(cors()); //to avoid disturbances in connectivity with cloud server
 app.use(express.json());
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
 
-const genres = [
-  { id: 1, genre: "action" },
-  { id: 2, genre: "thriller" },
-  { id: 3, genre: "comedy" },
-  { id: 4, genre: "romance" },
-];
+mongoose
+  .connect(
+    "mongodb://localhost/vidly",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    { useFindAndModify: false }
+  )
+  .then(() => console.log("database connected...!!!"))
+  .catch((err) => console.error("Could not connect to the database.."));
+
+const genreSchema = new mongoose.Schema({
+  genre: {
+    type: { String, Array },
+    _id : Object,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+const Genre = mongoose.model("Genre", genreSchema);
+
+const Movie = mongoose.model("Movies" , new mongoose.Schema({
+  title:{
+    type : String,
+    required: true,
+    trime: true,
+    minlength:5,
+    maxlength:255
+  },
+  genre:{
+    type: genreSchema,
+    required: true
+  },
+  numberInStock:{
+    type: Number,
+    required:true,
+    min: 0,
+    max: 255
+  },
+  dailyRentalRate:{
+    type: Number,
+    required: true,
+    min:0,
+    max:255
+  }
+}));
+
+//Customer Schema
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+  name:{
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  isGold:{
+    type: Boolean,
+    required: true
+  },
+  phone:{
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 10
+  },
+  address:{
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 100
+  }
+}))
+
+//Rental Schema
+const Rental = mongoose.model('Rental' , new mongoose.Schema({
+  customer:{
+    type: new mongoose.Schema({
+      name:{
+        type: String,
+        required: true,
+        ref : 'Customer',
+        minlength: 5,
+        maxlength: 50
+      },
+      isGold: {
+        type: Boolean,
+        required: true
+      },
+      phone:{
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      }
+    }),
+    required: true
+  },
+  movie:{
+    type: new mongoose.Schema({
+      title:{
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+      },
+      dailyRentalRate:{
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }
+    }),
+    required : true
+  },
+  dateOut:{
+    type: Date,
+    required : true,
+    default : Date.now
+  },
+  dateReturned:{
+    type: Date
+  },
+  rentalFee:{
+    type: Number,
+    min: 0
+  }
+}))
 
 //get the details of the genres available within the application vidly
-app.get("/vidly.com/api/genres", (req, res) => {
-  debugget("debugging GET method");
-  res.send(genres);
-});
+  app.get("/vidly.com/api/genres", async (req, res) => {
+    debugget("debugging GET method");
+    const genres = await Genre.find().sort("name");
+    res.send(genres);
+  });
 
 //post the new genre into the array
-app.post("/vidly.com/api/genres", (req, res) => {
+app.post("/vidly.com/api/genres", async (req, res) => {
   debugpost("debugging POST method");
-  var result = validateSchema.validatePostSchema(req.body);
+  var result = await validateSchema.validatePostSchema(req.body);
   if (result) {
     res.status(201).send(result);
   }
-  const genre = {
-    id: genres.length + 1,
-    genre: req.body.genre,
-  };
-
-  genres.push(genre);
-  res.send(genres);
+  let genre = new Genre({ genre: req.body });
+  genre = await genre.save();
+  res.send(genre);
 });
 
-app.put("/vidly.com/api/genres", (req, res) => {
+app.put("/vidly.com/api/genres", async (req, res) => {
   debugput("debugging PUT method");
-  var result = validateSchema.validateSchema(req.body);
+  var result =await validateSchema.validateSchema(req.body);
   if (result) {
     res.status(201).send(result);
   }
-  var genre = genres.find((gn) => gn.id === parseInt(req.body.id));
 
+  let genre = await Genre.findById(req.body.id);
+  if (genre) {
+    genre.genre = req.body.genre;
+    genre.save();
+  } else {
+    res.send("genre is empty");
+  }
   if (!genre) {
     return res.status(201).send("The id given as a request is not available");
   }
-
-  genre.genre = req.body.genre;
-  res.send(genres);
+  res.send(genre);
 });
 
 //DELETE
-app.delete("/vidly.com/api/genres", (req, res) => {
-  debugdelete("debugging DELETE method" , req.body);
-  var result = validateSchema.validateDeleteSchema(req.body);
+app.delete("/vidly.com/api/genres", async (req, res) => {
+  debugdelete("debugging DELETE method", req.body);
+  var result =await validateSchema.validateDeleteSchema(req.body);
   if (result) {
     res.status(201).send(result);
   }
-  var genre = genres.find((gn) => gn.id === parseInt(req.body.id));
+  const genre = await Genre.deleteOne({ _id: req.body.id });
   if (!genre) {
     return res.status(201).send("The id given as a request is not available");
   }
-  var index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.send(genres);
+
+  // genre.save();
+  res.send("The genre " + req.body.id + " is deleted");
+});
+
+
+//MOVIE API CALLS
+//GET
+app.get("/vidly.com/api/movies",async (req,res)=>{
+  const movies = await Movie.find().sort('title');
+  res.send(movies);
+})
+
+//POST
+app.post('/vidly.com/api/movies', async (req,res)=>{
+  var result = await validateSchema.validateMovie(req.body);
+  if (result) {
+    res.status(201).send(result);
+  }
+  const isValidId = mongoose.Types.ObjectId.isValid(req.body.genreId);
+  if(!isValidId) return res.status(400).send("Invalid genre");
+  const genre = await Genre.findById(req.body.genreId);
+    
+  let movie = await new Movie({ 
+    title: req.body.title,
+    genre:{
+      _id: genre._id,
+      genre:genre.genre
+    },
+    numberInStock:req.body.numberInStock,
+    dailyRentalRate:req.body.dailyRentalRate
+     });
+  movie = await movie.save();
+  await res.send(movie);
+})
+
+//PUT
+app.put("/vidly.com/api/movies", async (req, res) => {
+  debugput("debugging PUT method");
+  var result =await validateSchema.validateMovie(req.body);
+  if (result) {
+    res.status(201).send(result);
+  }
+
+  let movie = await Movie.findById(req.body.id);
+  let genre = await Genre.findById(req.body.genreId);
+  
+  if (movie && genre) {
+    movie.title = req.body.title;
+    movie.genre._id=genre._id;
+    movie.genre.genre=genre.genre;
+    movie.numberInStock= req.body.numberInStock;
+    movie.dailyRentalRate= req.body.dailyRentalRate;
+    movie.save();
+  } 
+  await res.send(movie);
+});
+
+//DELETE 
+app.delete("/vidly.com/api/movies", async (req, res) => {
+  debugdelete("debugging DELETE method", req.body);
+  var result =await validateSchema.validateMovieDelete(req.body);
+  if (result) {
+    res.status(201).send(result);
+  }
+  const movie = await Movie.deleteOne({ _id: req.body.id });
+  if (!movie) {
+    return res.status(201).send("The id given as a request is not available");
+  }
+
+  res.send("The movie " + req.body.id + " is deleted");
+});
+
+//Customer API Calls
+app.get("/vidly.com/api/customers", async (req, res)=>{
+  const customers = await Customer.find().sort('name');
+  res.send(customers);
+})
+
+app.post("/vidly.com/api/customers" , async(req, res)=>{
+  var result = await validateSchema.validateCustomer(req.body);
+  if (result) {
+    res.status(201).send(result);
+  } 
+  let customer = await new Customer({ 
+    name: req.body.name,
+    isGold: req.body.isGold,
+    phone: req.body.phone,
+    address: req.body.address
+    });
+  customer = await customer.save();
+  await res.send(customer);
+})
+
+//PUT
+app.put("/vidly.com/api/customers", async (req, res) => {
+  debugput("debugging PUT method");
+  var result =await validateSchema.validateCustomerUpdate(req.body);
+  if (result) {
+    res.status(201).send(result);
+  }
+
+  let customer = await Customer.findById(req.body.id);
+  if (customer) {
+    customer.name = req.body.name,
+    customer.isGold=req.body.isGold,
+    customer.phone=req.body.phone,
+    customer.address=req.body.address
+    customer.save();
+  } else {
+    res.send("customer is empty");
+  }
+  await res.send(customer);
+});
+
+app.delete("/vidly.com/api/customers", async (req, res)=>{
+    debugdelete("debugging DELETE method", req.body);
+    var result =await validateSchema.validateCustomerDelete(req.body);
+    if (result) {
+      res.status(201).send(result);
+    }
+    const customer = await Customer.deleteOne({ _id: req.body.id });
+    if (!customer) {
+      return res.status(201).send("The id given as a request is not available");
+    }
+  
+    res.send("The customer " + req.body.id + " is deleted");
+});
+
+//RENTAL API CALLS
+//GET
+app.get("/vidly.com/api/rentals", async (req,res)=>{
+  const rentals = await Rental.find().sort('-dateOut');
+  res.send(rentals);
+})
+
+//POST
+app.post("/vidly.com/api/rentals", async (req,res)=>{
+  var result = await validateSchema.validateRental(req.body);
+  if (result) {
+    res.status(201).send(result);
+  }
+  let isValidId = mongoose.Types.ObjectId.isValid(req.body.customerId);
+  if(!isValidId) return res.status(400).send("Invalid customer");
+  const customer = await Customer.findById(req.body.customerId);
+  
+  isValidId = mongoose.Types.ObjectId.isValid(req.body.movieId);
+  if(!isValidId) return res.status(400).send("Invalid movie");
+  let movie = await Movie.findById(req.body.movieId);
+  
+      
+  let rental = await new Rental({ 
+    customer:{
+      _id: customer._id,
+      name: customer.name,
+      isGold: customer.isGold,
+      phone:customer.phone
+    },
+    movie:{
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate:movie.dailyRentalRate
+    },
+    dateOut: req.body.dateOut,
+    dateReturned: req.body.dateReturned,
+    rentalFee:req.body.rentalFee
+     });
+  rental = await rental.save();
+  if(rental){
+    console.log("stock ",movie.numberInStock);
+    movie.numberInStock = movie.numberInStock-1;
+    movie.save();
+    
+  }
+  await res.send(rental);
+})
+
+//PUT
+app.put("/vidly.com/api/rentals", async (req,res)=>{
+  var result = await validateSchema.validateRentalUpdate(req.body);
+  if (result) {
+    res.status(201).send(result);
+  }
+  let isValidId = mongoose.Types.ObjectId.isValid(req.body.customerId);
+  if(!isValidId) return res.status(400).send("Invalid customer");
+  const customer = await Customer.findById(req.body.customerId);
+  
+  isValidId = mongoose.Types.ObjectId.isValid(req.body.movieId);
+  if(!isValidId) return res.status(400).send("Invalid movie");
+  const movie = await Movie.findById(req.body.movieId);
+
+  isValidId = mongoose.Types.ObjectId.isValid(req.body.id);
+  if(!isValidId) return res.status(400).send("Invalid rental");
+  const rental = await Rental.findById(req.body.id);
+      
+  if (rental) {
+    rental.customer._id=req.body.customerId
+    rental.customer.name=customer.name,
+    rental.customer.isGold=customer.isGold,
+    rental.customer.phone=customer.phone
+    
+    rental.movie._id=req.body.movieId,
+    rental.movie.title=movie.title,
+    rental.movie.dailyRentalRate = movie.dailyRentalRate
+    
+    rental.dateOut=req.body.dateOut,
+    rental.dateReturned=req.body.dateReturned,
+    rental.rentalFee=req.body.rentalFee
+    rental.save();
+  } else {
+    res.send("Rental is empty");
+  }
+  await res.send(rental);
+})
+
+//DELETE
+app.delete("/vidly.com/api/rentals", async (req, res)=>{
+  debugdelete("debugging DELETE method", req.body);
+  var result = await validateSchema.validateRentalDelete(req.body);
+  if (result) {
+    res.status(201).send(result);
+  }
+  const rental = await Rental.deleteOne({ _id: req.body.id });
+  if (!rental) {
+    return res.status(201).send("The id given as a request is not available");
+  }
+
+  await res.send("The rental " + req.body.id + " is deleted");
 });
 
 const port = process.env.PORT || 3000;
--- validations/validation.js
+++ validations/validation.js
@@ -1,38 +1,155 @@
 const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
 
-function validateSchema(requestBody) {
-  const requestBodySchema = Joi.object().keys({
-    id: Joi.number().required(),
+async function validateSchema(requestBody) {
+  const requestBodySchema = await Joi.object().keys({
+    id: Joi.required(),
     genre: Joi.string().required(),
   });
 
-  const { error } = requestBodySchema.validate(requestBody);
+  const { error } = await requestBodySchema.validate(requestBody);
 
   if (error) {
     return error.details[0].message;
   }
 }
 
-function validateDeleteSchema(requestBody) {
- const requestBodyDeleteSchema = Joi.object().keys({
-    id: Joi.number().required(),
+async function validateDeleteSchema(requestBody) {
+ const requestBodyDeleteSchema =await  Joi.object().keys({
+    id: Joi.required(),
   });
 
-  const { error } = requestBodyDeleteSchema.validate(requestBody);
+  const { error } = await requestBodyDeleteSchema.validate(requestBody);
 
   if (error) {
     return error.details[0].message;
   }
 }
 
-function validatePostSchema(requestBody) {
-  const requestBodyPostSchema = Joi.object().keys({
+async function validatePostSchema(requestBody) {
+  const requestBodyPostSchema =await Joi.object().keys({
     genre: Joi.string().required(),
   });
-  const { error } = requestBodyPostSchema.validate(requestBody);
+  const { error } =await  requestBodyPostSchema.validate(requestBody);
   if (error) {
     return error.details[0].message;
   }
 }
 
-module.exports = { validateSchema, validateDeleteSchema, validatePostSchema };
+async function validateMovie(movie){
+  const schema=await Joi.object().keys({
+    title:  Joi.string().required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().required(),
+    dailyRentalRate: Joi.number().required() 
+  });
+  const {error} = await schema.validate(movie);
+  if(error){
+    return error.details[0].mesaage;
+  }
+}
+
+async function validateMovieUpdate(movie){
+  const schema=await Joi.object().keys({
+    id:Joi.required(),
+    title:  Joi.string().required(),
+    genreId: Joi.string().required(),
+    genreName:Joi.string().required(),
+    numberInStock: Joi.number().required(),
+    dailyRentalRate: Joi.number().required() 
+  });
+  const {error} = await schema.validate(movie);
+  if(error){
+    return error.details[0].mesaage;
+  }
+}
+
+async function validateMovieDelete(movie){
+  const schema=await Joi.object().keys({
+    id:Joi.required()
+  });
+  const {error} = await schema.validate(movie);
+  if(error){
+    return error.details[0].mesaage;
+  }
+}
+
+async function validateRental(rental){
+  console.log(rental);
+  const schema = await Joi.object().keys({
+    customerId:Joi.string().required(),
+    movieId:Joi.string().required(),
+    dateOut: Joi.required(),
+    dateReturned: Joi.required(),
+    rentalFee:Joi.number().required()
+  });
+  const {error} = await schema.validate(rental);
+  if(error){
+    return error.details[0].message;
+  }
+}
+
+async function validateRentalUpdate(rental){
+  const schema = await Joi.object().keys({
+    id: Joi.string().required(),
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+    dateOut:Joi.required(),
+    dateReturned:Joi.required(),
+    rentalFee:Joi.number().required()
+  });
+  const {error} = await schema.validate(rental);
+  if(error){
+    return error.details[0].message;
+  }
+}
+
+async function validateRentalDelete(rental){
+  const schema=await Joi.object().keys({
+    id:Joi.required()
+  });
+  const {error} = await schema.validate(rental);
+  if(error){
+    return error.details[0].mesaage;
+  }
+}
+
+async function validateCustomer(customer){
+  const schema = await Joi.object().keys({
+    name:Joi.string().required(),
+    isGold: Joi.required(),
+    phone:Joi.string().required(),
+    address:Joi.string().required()
+  });
+  const {error} = await schema.validate(customer);
+  if(error){
+    return error.details[0].message;
+  }
+}
+
+async function validateCustomerUpdate(customer){
+  const schema = await Joi.object().keys({
+    id:Joi.string().required(),
+    name:Joi.string().required(),
+    isGold: Joi.required(),
+    phone:Joi.string().required(),
+    address:Joi.string().required()
+  });
+  const {error} = await schema.validate(customer);
+  if(error){
+    return error.details[0].message;
+  }
+}
+
+async function validateCustomerDelete(customer){
+  const schema=await Joi.object().keys({
+    id:Joi.required()
+  });
+  const {error} = await schema.validate(customer);
+  if(error){
+    return error.details[0].mesaage;
+  }
+}
+
+
+module.exports = { validateSchema, validateDeleteSchema, validatePostSchema ,validateMovie, validateMovieUpdate, validateMovieDelete, validateRental, validateRentalUpdate, validateRentalDelete, validateCustomer, validateCustomerUpdate, validateCustomerDelete};
